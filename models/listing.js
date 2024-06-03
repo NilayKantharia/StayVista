b49d8d053670226b82796d72db6546375d6e068b
Nilay <nilaykanthariya@gmail.com>
2024-06-03 22:19:06 +0530

    set default value of image

--- models/listing.js
+++ models/listing.js
@@ -9,6 +9,7 @@ const listingSchema = new Schema({
     description: String,
     image: {
         type:String,
+        default:"https://images.unsplash.com/photo-1559333086-b0a56225a93c?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         set : (v) => {
             v === '' ? "https://images.unsplash.com/photo-1559333086-b0a56225a93c?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v
         }
