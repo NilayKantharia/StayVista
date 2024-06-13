b9f8efad9e172ed34e57a5597a13694e1d378a08
Nilay <nilaykanthariya@gmail.com>
2024-06-13 23:48:32 +0530

    added some routes

--- index.js
+++ index.js
@@ -5,16 +5,19 @@ const {connectToMongo} = require('./connection');
 const path = require('path');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 const Listing = require('./models/listing');
 
 connectToMongo(process.env.mongoDBURL).then(() => console.log('MongoDB connected'));
 
 app.set("view engine", "ejs");
+app.engine('ejs',ejsMate)
 app.set("views", path.join(__dirname, "views"));
 
 
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get('/', (req, res) => {
     res.send("working!");
