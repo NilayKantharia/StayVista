93ab2a93ba1b1549ed99516c116d11779725f688
Nilay <nilaykanthariya@gmail.com>
2024-06-15 12:46:45 +0530

    joi object validation

---
+++ schema.js
@@ -0,0 +1,10 @@
+const Joi = require('joi');
+
+module.exports.listingSchema = Joi.object({
+    title : Joi.string().required(),
+    description : Joi.string().required(),
+    location: Joi.string().required(),
+    country: Joi.string().required(),
+    price: Joi.number().required().min(0),
+    image: Joi.string().allow("", null)
+})
