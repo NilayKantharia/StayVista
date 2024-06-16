4259b8829e1e23528938d62c11565badf1fb746a
Nilay <nilaykanthariya@gmail.com>
2024-06-16 23:49:54 +0530

    added review schema joi object

--- schema.js
+++ schema.js
@@ -8,3 +8,10 @@ module.exports.listingSchema = Joi.object({
     price: Joi.number().required().min(0),
     image: Joi.string().allow("", null)
 })
+
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        rating: Joi.number().required().min(1).max(5),
+        comment: Joi.string().required()
+    }).required()
+})
