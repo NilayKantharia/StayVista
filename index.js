b5109a663ad4651d6d205239a05d161b6fe25e9c
Nilay <nilaykanthariya@gmail.com>
2024-06-19 22:19:46 +0530

    implemented user route

--- index.js
+++ index.js
@@ -8,10 +8,14 @@ const ejsMate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressError.js');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+const localStrategy = require('passport-local');
+const User = require('./models/user.js');
 
 
-const listings = require('./routes/listing.js');
-const reviews = require('./routes/review.js')
+const listingRouter = require('./routes/listing.js');
+const reviewRouter = require('./routes/review.js')
+const userRouter = require('./routes/user.js');
 
 connectToMongo(process.env.mongoDBURL).then(() => console.log('MongoDB connected'));
 
@@ -42,14 +46,31 @@ app.get('/', (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
 })
 
-app.use('/listing',listings);
-app.use('/listing/:id/reviews', reviews);
+app.get("/demouser", async (req, res) => {
+    let fakeUser = new User({
+        email: "student@gmail.com",
+        username: "demo-student"
+    });
+    let registeredUser = await User.register(fakeUser, "helloworld");
+    res.send(registeredUser);
+})
+
+app.use('/listing',listingRouter);
+app.use('/listing/:id/reviews', reviewRouter);
+app.use('/', userRouter);
 
 
 app.all('*', (req, res, next) => {
