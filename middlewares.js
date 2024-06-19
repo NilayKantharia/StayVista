a9d6a8e142964409b11b419d5923824b3b27d274
Nilay <nilaykanthariya@gmail.com>
2024-06-19 23:49:34 +0530

    added dynamic login-logout options

--- middlewares.js
+++ middlewares.js
@@ -1,7 +1,15 @@
 module.exports.isLoggedIn = (req, res, next) => {
     if(!req.isAuthenticated()){
+        req.session.redirectURL = req.originalUrl;
         req.flash("error", "you must be logged in first!");
         return res.redirect("/login");
+    }
+    next();
+}
+
+module.exports.saveRedirectUrl = (req, res, next) => {
+    if(req.session.redirectURL){
+        res.locals.redirectURL = req.session.redirectURL;
     }
     next();
 }
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require('../models/user');
 const wrapAsync = require('../utils/wrapAsync');
 const passport = require('passport');
+const { saveRedirectUrl } = require('../middlewares');
 
 router.get('/signup', (req, res) => {
     res.render('users/signup');
@@ -13,8 +14,13 @@ router.post('/signup', wrapAsync(async (req, res) => {
         let {username, email, password} = req.body;
         const newUser = new User({email, username});
         const registeredUser = await User.register(newUser, password);
-        req.flash("success", "Welcome to StayVista!");
-        res.redirect("/listing");
+        req.login(registeredUser, (err) => {
+            if(err){
+                return next(err);
+            }
+            req.flash("success", "Welcome to StayVista!");
+            res.redirect("/listing");
+        })
     }catch(e){
         req.flash("error", e.message);
         res.redirect("/signup");
@@ -27,9 +33,9 @@ router.get('/login', (req, res) => {
     res.render('users/login');
 });
 
-router.post('/login', passport.authenticate("local", {failureRedirect: '/login', failureFlash: true}), async (req, res) => {
-    req.flash("success","Welcome to stayvista");
-    res.redirect("/listing");
+router.post('/login', saveRedirectUrl, passport.authenticate("local", {failureRedirect: '/login', failureFlash: true}), async (req, res) => {
+    req.flash("success","Welcome to StayVista");
+    res.redirect(res.locals.redirectURL || "/listing");
 })
 
 //logout
