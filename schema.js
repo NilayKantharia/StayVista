ca7d5da56a0b368df25f9f966ac06f9fdcef17bf
Nilay <nilaykanthariya@gmail.com>
2024-06-27 00:23:23 +0530

    updated validation according to category

--- schema.js
+++ schema.js
@@ -4,6 +4,7 @@ module.exports.listingSchema = Joi.object({
     title : Joi.string().required(),
     description : Joi.string().required(),
     location: Joi.string().required(),
+    category: Joi.string().allow("", null),
     country: Joi.string().required(),
     price: Joi.number().required().min(0),
     image: Joi.string().allow("", null)
